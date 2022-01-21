0330e1ca8dff9d5c8b46cf90ce451063916de7a2
Moises Junior <m.junior@eq.ufrj.br>
2022-01-21 00:18:29 -0300

    Rotas post e get feitas

---
+++ back-end/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+let user = {};
+let tweets = [];
+
+server.post('/sign-up', (req, res) => {
+    user = req.body;
+    res.send("OK")
+})
+
+server.post('/tweets', (req, res) => {
+    tweets.push({...req.body, avatar: user.avatar});
+    res.send("OK")
+})
+
+server.get('/tweets', (req, res) => {
+    res.send(tweets)
+})
+
+server.listen(5000);
