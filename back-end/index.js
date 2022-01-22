b142914327b6277d48f44dc619f281648eb6b5a4
Moises Junior <m.junior@eq.ufrj.br>
2022-01-22 17:32:56 -0300

    Bug do avatar corrigido

--- back-end/index.js
+++ back-end/index.js
@@ -5,14 +5,14 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-let user = {};
+let users = [];
 let tweets = [];
 
 server.post('/sign-up', (req, res) => {
     if (req.body.username === "" || req.body.avatar === "") {
         res.status(400).send("Todos os campos são obrigatórios!")
     } else {
-        user = req.body;
+        users.push(req.body);
         res.status(201).send("OK")
     }
 })
@@ -20,18 +20,20 @@ server.post('/sign-up', (req, res) => {
 server.post('/tweets', (req, res) => {
     const tweet = req.body.tweet;
     const username = req.headers.user;
+    const avatar = users.find(user => user.username === username).avatar;
    
     if (tweet === "" || username === "") {
         res.status(400).send("Todos os campos são obrigatórios!")
     } else {
         if (tweets.length < 10) {
-            tweets.unshift({username, avatar: user.avatar, tweet});
+            tweets.unshift({username, avatar, tweet});
         } else {
             tweets.pop();
-            tweets.unshift({username, avatar: user.avatar, tweet});
+            tweets.unshift({username, avatar, tweet});
         }
         res.status(201).send("OK")
     }
+    console.log(avatar)
 })
 
 server.get('/tweets', (req, res) => {
